d1c2b8b2d7043bede666439fb5dd68a413772a5c
Vijay Shirsath <112496633+vijay-shirsath@users.noreply.github.com>
2024-03-07 17:33:49 +0530

    Delete server directory

--- server/routes/Answers.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-
-import {deleteAnswer, postAnswer} from "../controllers/Answers.js"
-const router = express.Router();
-
-router.patch("/post/:id", postAnswer);
-router.patch("/delete/:id",deleteAnswer);
-export default router;
